1fe17df3927f13acbe155578b476e6175de451b2
Bryce Hills <brycehills@Bryces-MacBook-Pro.local>
2025-05-27 17:40:27 -0700

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-blue-600 text-4xl font-bold">
+        ✅ Tailwind text-blue-600 is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
